1bde021afc41d4c2f0479586a19f238889ff92da
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-11-26 01:35:38 +0100

    Add NoneAware attribute access

--- Python/symtable.c
+++ Python/symtable.c
@@ -2524,6 +2524,9 @@ symtable_visit_expr(struct symtable *st, expr_ty e)
     case Constant_kind:
         /* Nothing to do here. */
         break;
+    case NoneAwareAttribute_kind:
+        VISIT(st, expr, e->v.NoneAwareAttribute.value);
+        break;
     /* The following exprs can be assignment targets. */
     case Attribute_kind:
         if (!check_name(st, e->v.Attribute.attr, LOCATION(e), e->v.Attribute.ctx)) {
