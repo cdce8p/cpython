7c9416dedfaa14a9a1ad7152787a5e15676c39d0
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2025-12-28 10:41:18 +0100

    Add Maybe

--- Include/internal/pycore_ast.h
+++ Include/internal/pycore_ast.h
@@ -365,12 +365,12 @@ enum _expr_kind {BoolOp_kind=1, NamedExpr_kind=2, BinOp_kind=3, UnaryOp_kind=4,
                   Lambda_kind=5, IfExp_kind=6, Dict_kind=7, Set_kind=8,
                   ListComp_kind=9, SetComp_kind=10, DictComp_kind=11,
                   GeneratorExp_kind=12, NoneAwareAttribute_kind=13,
-                  NoneAwareSubscript_kind=14, Await_kind=15, Yield_kind=16,
-                  YieldFrom_kind=17, Compare_kind=18, Call_kind=19,
-                  FormattedValue_kind=20, Interpolation_kind=21,
-                  JoinedStr_kind=22, TemplateStr_kind=23, Constant_kind=24,
-                  Attribute_kind=25, Subscript_kind=26, Starred_kind=27,
-                  Name_kind=28, List_kind=29, Tuple_kind=30, Slice_kind=31};
+                  NoneAwareSubscript_kind=14, Maybe_kind=15, Await_kind=16,
+                  Yield_kind=17, YieldFrom_kind=18, Compare_kind=19,
+                  Call_kind=20, FormattedValue_kind=21, Interpolation_kind=22,
+                  JoinedStr_kind=23, TemplateStr_kind=24, Constant_kind=25,
+                  Attribute_kind=26, Subscript_kind=27, Starred_kind=28,
+                  Name_kind=29, List_kind=30, Tuple_kind=31, Slice_kind=32};
 struct _expr {
     enum _expr_kind kind;
     union {
@@ -445,6 +445,10 @@ struct _expr {
             expr_ty value;
             expr_ty slice;
         } NoneAwareSubscript;
+
+        struct {
+            expr_ty value;
+        } Maybe;
 
         struct {
             expr_ty value;
@@ -843,6 +847,8 @@ expr_ty _PyAST_NoneAwareAttribute(expr_ty value, identifier attr, int group,
 expr_ty _PyAST_NoneAwareSubscript(expr_ty value, expr_ty slice, int group, int
                                   lineno, int col_offset, int end_lineno, int
                                   end_col_offset, PyArena *arena);
+expr_ty _PyAST_Maybe(expr_ty value, int group, int lineno, int col_offset, int
+                     end_lineno, int end_col_offset, PyArena *arena);
 expr_ty _PyAST_Await(expr_ty value, int group, int lineno, int col_offset, int
                      end_lineno, int end_col_offset, PyArena *arena);
 expr_ty _PyAST_Yield(expr_ty value, int group, int lineno, int col_offset, int
