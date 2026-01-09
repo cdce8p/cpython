4688b78e26505e6b7e2a8a5d1052917ff3857c4f
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2026-01-09 18:33:23 +0100

    Add pseudo instructions JUMP_IF_NONE/NOT_NONE

--- Python/codegen.c
+++ Python/codegen.c
@@ -5968,8 +5968,7 @@ codegen_none_aware_attribute(compiler *c, expr_ty e)
 
     VISIT(c, expr, e->v.NoneAwareAttribute.value);
     _PyCompile_PopNATarget(c);
-    ADDOP_I(c, loc, COPY, 1);
-    ADDOP_JUMP(c, loc, POP_JUMP_IF_NONE, next);
+    ADDOP_JUMP(c, loc, JUMP_IF_NONE, next);
     ADDOP_NAME(c, loc, LOAD_ATTR, e->v.NoneAwareAttribute.attr, names);
 
     if (use_jump_target == 1) {
@@ -5992,8 +5991,7 @@ codegen_none_aware_subscript(compiler *c, expr_ty e)
 
     VISIT(c, expr, e->v.NoneAwareSubscript.value);
     _PyCompile_PopNATarget(c);
-    ADDOP_I(c, loc, COPY, 1);
-    ADDOP_JUMP(c, loc, POP_JUMP_IF_NONE, next);
+    ADDOP_JUMP(c, loc, JUMP_IF_NONE, next);
 
     _PyCompile_PushNABlock(c, loc);
     if (should_apply_two_element_slice_optimization(e->v.NoneAwareSubscript.slice)) {
