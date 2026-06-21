c0b89122ef530a760e4076e68284e6bc88067ea1
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2026-06-21 11:40:32 +0200

    Merge CoalesceOp and BoolOp

--- Include/internal/pycore_ast_state.h
+++ Include/internal/pycore_ast_state.h
@@ -42,7 +42,8 @@ struct ast_state {
     PyObject *Call_type;
     PyObject *ClassDef_type;
     PyObject *CoalesceAssign_type;
-    PyObject *CoalesceOp_type;
+    PyObject *Coalesce_singleton;
+    PyObject *Coalesce_type;
     PyObject *Compare_type;
     PyObject *Constant_type;
     PyObject *Continue_type;
